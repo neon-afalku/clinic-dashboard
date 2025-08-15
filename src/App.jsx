bd298831c675fee031e1e94cda726a5d3c3d0a9f
super user1 <saidanis.khouider@outlook.fr>
2025-08-15 09:47:00 +0100

    Creating the main architecture of the clinic dashboard project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+/*
+Dashboard clinique qui fera les choses suivantes :
+-Création de comptes d'authentication (Pages Login et Register)
+-Création du dashboard qui va afficher les informations de la clinique
+-Page de profil de l'utilisateur authentifié
+-Page 404 Not found
+*/
+import React from 'react';
+
+function App(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Error404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Error404(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Error404;
